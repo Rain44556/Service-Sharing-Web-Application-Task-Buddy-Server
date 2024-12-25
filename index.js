cb3c68017876eea5f44b715a8ea492c9f7aa748d
rain44556 <bristy44556@gmail.com>
2024-12-26 00:57:16 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +9,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0czr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,7 +27,22 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
 
+  if (!token) {
+      return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  // verify the token
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'unauthorized access' });
+      }
+      req.user = decoded;
+      next();
+  })
+}
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,21 +53,37 @@ async function run() {
   
 
 
-
-
   const servicesCollection = client.db('ServiceSharingDB').collection('services');
-  const userCollection = client.db('usersDB').collection('users');
+  const userCollection = client.db('ServiceSharingDB').collection('users');
+  const bookCollection = client.db('ServiceSharingDB').collection('booking');
+  
+
+    //---------Auth api--------//
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
+      });
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({ success: true})
+    })
+
 
      //---------users api--------//
      app.post('/users', async (req, res) => {
       const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
       res.send(result);
-    })
+    });
+
+
 
   //---------------services apis-----------//
 
-  app.get('/services', async(req,res)=>{
+  app.get('/allServices', async(req,res)=>{
     const cursor = servicesCollection.find();
     const result = await cursor.toArray();
     res.send(result);
@@ -57,14 +94,10 @@ async function run() {
     res.send(result);
   })
 
-  app.get('/services/:id', async(req,res)=>{
+  app.get('/services/:id', verifyToken, async(req,res)=>{
     const id = req.params.id;
     const query = { _id: new ObjectId(id) }
     const result = await servicesCollection.findOne(query);
-
-    // const {serviceProviderEmail} = result;
-    // const userInformation = await userCollection.findOne({email:serviceProviderEmail});
-    // result.providerInformation = userInformation;
     
     res.send(result);
   });
@@ -77,9 +110,9 @@ async function run() {
   });
 
   
-  app.get('/service', async (req, res) => {
+  app.get('/userServices', async (req, res) => {
     const email = req.query.email;
-    const query = { providerEmail: email };
+    const query = { "serviceProvider.providerEmail" : email };
     const cursor =  servicesCollection.find(query);
     const result = await cursor.toArray();
     res.send(result);
@@ -92,10 +125,7 @@ async function run() {
     res.send(result);
   })
 
-  
 
- 
-  
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
